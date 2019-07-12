7d367910a7945bf4768d24bae873a6942f0b53bf
Gabriel <eu@gabrielf.com>
2019-07-12 00:56:41 -0300

    Adding the queries of the project

--- 1-data-modeling/L3-Project_Data_Modeling_with_Postgres/sql_queries.py
+++ 1-data-modeling/L3-Project_Data_Modeling_with_Postgres/sql_queries.py
@@ -1,26 +1,67 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS `songplays`"
+user_table_drop = "DROP TABLE IF EXISTS `users`"
+song_table_drop = "DROP TABLE IF EXISTS `songs`"
+artist_table_drop = "DROP TABLE IF EXISTS `artists`"
+time_table_drop = "DROP TABLE IF EXISTS `time`"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS `songplays` (
+        `songplay_id` INTEGER ,
+        `start_time` TIMESTAMP NOT NULL,
+        `user_id` INTEGER NOT NULL REFERENCES `users` (`user_id`),
+        `level` NUMERIC (15, 5),
+        `song_id` INTEGER NOT NULL REFERENCES `songs` (`song_id`),
+        `artist_id` VARCHAR NOT NULL REFERENCES `artists` (`artist_id`),
+        `session_id` INTEGER NOT NULL,
+        `location` VARCHAR,
+        `user_agent` VARCHAR
+    )
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS `users` (
+        `user_id` INTEGER PRIMARY KEY,
+        `first_name` VARCHAR NOT NULL,
+        `last_name` VARCHAR NOT NULL,
+        `gender` CHAR(1),
+        `level` VARCHAR NOT NULL
+    )
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS `songs` (
+        `song_id` VARCHAR PRIMARY KEY,
+        `title` VARCHAR NOT NULL,
+        `artist_id` VARCHAR NOT NULL REFERENCES `artists` (`artist_id`),
+        `year` INTEGER NOT NULL,
+        `duration` NUMERIC (15, 5) NOT NULL
+    )
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS `artists` (
+        `artist_id` VARCHAR PRIMARY KEY,
+        `name` VARCHAR NOT NULL,
+        `location` VARCHAR,
+        `latitude` NUMERIC,
+        `longitude` NUMERIC
+    )
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS `time` (
+        `start_time` TIMESTAMP NOT NULL PRIMARY KEY,
+        `hour` NUMERIC NOT NULL,
+        `day` NUMERIC NOT NULL,
+        `week` NUMERIC NOT NULL,
+        `month` NUMERIC NOT NULL,
+        `year` NUMERIC NOT NULL,
+        `weekday` NUMERIC NOT NULL
+    )
 """)
 
 # INSERT RECORDS
