123d1c7b461be4fb1ce43aab73e7e6970b94f728
Gabriel <eu@gabrielf.com>
2019-07-25 11:49:31 -0300

    create tables queries first version :)

--- 2-cloud-data-warehouses/L4_Project_-_Data_Warehouse/sql_queries.py
+++ 2-cloud-data-warehouses/L4_Project_-_Data_Warehouse/sql_queries.py
@@ -7,8 +7,8 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
 songplay_table_drop = ""
 user_table_drop = ""
 song_table_drop = ""
@@ -18,24 +18,97 @@ time_table_drop = ""
 # CREATE TABLES
 
 staging_events_table_create= ("""
+    CREATE TABLE staging_events (
+        artist VARCHAR(255),
+        auth VARCHAR(20),
+        firstName VARCHAR(255),
+        gender CHAR(1),
+        itemInSession INTEGER,
+        lastName VARCHAR(255),
+        length DECIMAL(12, 5),
+        level VARCHAR(10),
+        location VARCHAR(255),
+        method VARCHAR(20),
+        page VARCHAR(255),
+        registration VARCHAR(255),
+        sessionId INTEGER,
+        song VARCHAR(255),
+        status INTEGER,
+        ts TIMESTAMP,
+        userAgent VARCHAR(255),
+        userId INTEGER
+    );
 """)
 
 staging_songs_table_create = ("""
+    CREATE TABLE staging_songs (
+        num_songs INTEGER,
+        artist_id VARCHAR(20),
+        artist_latitude DECIMAL(5, 8),
+        artist_longitude DECIMAL(5, 8),
+        artist_location VARCHAR(255),
+        artist_name VARCHAR(255),
+        song_id VARCHAR(20),
+        title VARCHAR(255),
+        duration DECIMAL(12, 5),
+        year INTEGER
+    );
 """)
 
 songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays (
+        songplay_id INTEGER IDENTITY(0,1) SORTKEY,
+        start_time INTEGER NOT NULL,
+        user_id INTEGER NOT NULL REFERENCES users (user_id),
+        level VARCHAR(10),
+        song_id VARCHAR(20) REFERENCES songs (song_id),
+        artist_id VARCHAR(20) REFERENCES artists (artist_id),
+        session_id INTEGER NOT NULL,
+        location VARCHAR(255),
+        user_agent VARCHAR(255)
+    )
 """)
 
 user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id INTEGER PRIMARY KEY,
+        first_name VARCHAR(50) NOT NULL,
+        last_name VARCHAR(50) NOT NULL,
+        gender CHAR(1),
+        level VARCHAR(10) NOT NULL
+    )
 """)
 
 song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs (
+        song_id VARCHAR(20) PRIMARY KEY,
+        title VARCHAR(255) NOT NULL SORTKEY,
+        artist_id VARCHAR NOT NULL DISTKEY REFERENCES artists (artist_id),
+        year INTEGER NOT NULL,
+        duration DECIMAL (15, 5) NOT NULL
+    )
 """)
 
 artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists (
+        artist_id VARCHAR(20) PRIMARY KEY,
+        name VARCHAR(100) NOT NULL SORTKEY,
+        location VARCHAR(255),
+        latitude DECIMAL(4,8),
+        longitude DECIMAL(4,8)
+    )
 """)
 
 time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time (
+        start_time TIMESTAMP NOT NULL PRIMARY KEY SORTKEY,
+        hour NUMERIC NOT NULL,
+        day NUMERIC NOT NULL,
+        week NUMERIC NOT NULL,
+        month NUMERIC NOT NULL,
+        year NUMERIC NOT NULL,
+        weekday NUMERIC NOT NULL
+    )
 """)
 
 # STAGING TABLES
