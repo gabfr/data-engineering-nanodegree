bd3bd6b5361565ef50b0f2a27dbc473a3e493ac7
Gabriel <eu@gabrielf.com>
2019-07-26 11:34:52 -0300

    When cluster available, then update the config and the firewall options

--- 2-cloud-data-warehouses/L4_Project_-_Data_Warehouse/check_cluster_available.py
+++ 2-cloud-data-warehouses/L4_Project_-_Data_Warehouse/check_cluster_available.py
@@ -1,4 +1,4 @@
-from create_cluster import config_parse_file, aws_client, check_cluster_creation
+from create_cluster import config_parse_file, aws_client, aws_open_redshift_port, check_cluster_creation, aws_resource, config_persist_cluster_infos
 
 def main():
     config_parse_file()
@@ -7,6 +7,9 @@ def main():
 
     if check_cluster_creation(redshift):
         print('available')
+        ec2 = aws_resource('ec2', 'us-east-2')
+        config_persist_cluster_infos(redshift)
+        aws_open_redshift_port(ec2, redshift)
     else:
         print('notyet')
 
