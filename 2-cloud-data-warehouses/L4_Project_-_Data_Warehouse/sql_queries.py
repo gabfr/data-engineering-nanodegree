38963e65989406c87325d09360dc55750a1facd4
Gabriel <eu@gabrielf.com>
2019-07-26 12:29:06 -0300

    Empty queries shouldnt exist

--- 2-cloud-data-warehouses/L4_Project_-_Data_Warehouse/sql_queries.py
+++ 2-cloud-data-warehouses/L4_Project_-_Data_Warehouse/sql_queries.py
@@ -14,11 +14,11 @@ DWH_IAM_ROLE_ARN = config.get("IAM_ROLE", "ARN")
 
 staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
 staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs;"
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
 
 # CREATE TABLES
 
