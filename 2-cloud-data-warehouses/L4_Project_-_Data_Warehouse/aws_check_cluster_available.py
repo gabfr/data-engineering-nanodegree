f8c3827100b782f4c268f629e1eac77cd880e515
Gabriel <eu@gabrielf.com>
2019-07-29 09:22:59 -0300

    Back to region `us-east-2`

--- 2-cloud-data-warehouses/L4_Project_-_Data_Warehouse/aws_check_cluster_available.py
+++ 2-cloud-data-warehouses/L4_Project_-_Data_Warehouse/aws_check_cluster_available.py
@@ -3,11 +3,11 @@ from aws_create_cluster import config_parse_file, aws_client, aws_open_redshift_
 def main():
     config_parse_file()
 
-    redshift = aws_client('redshift', "us-west-2")
+    redshift = aws_client('redshift', "us-east-2")
 
     if check_cluster_creation(redshift):
         print('available')
-        ec2 = aws_resource('ec2', 'us-west-2')
+        ec2 = aws_resource('ec2', 'us-east-2')
         config_persist_cluster_infos(redshift)
         aws_open_redshift_port(ec2, redshift)
     else:
