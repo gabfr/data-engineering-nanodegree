d818e60e80d3c91aa7c2b1c8de6d5a43740d056f
Gabriel <eu@gabrielf.com>
2019-07-30 10:35:19 -0300

    Creating an analyze.py script to help us check the results

---
+++ 2-cloud-data-warehouses/L4_Project_-_Data_Warehouse/analyze.py
@@ -0,0 +1,29 @@
+import configparser
+import psycopg2
+from sql_queries import analytical_queries, analytical_query_titles
+
+
+def run_analytical_queries(cur):
+    idx = 0
+    for query in analytical_queries:
+        print("{}... ".format(analytical_query_titles[idx]))
+        row = cur.execute(query)
+        print(row.total)
+        idx = idx + 1
+        print("  [DONE]  ")
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+
+    run_analytical_queries(cur)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
