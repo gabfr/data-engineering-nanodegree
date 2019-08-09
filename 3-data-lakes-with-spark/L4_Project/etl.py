37b8dd5f0aaaa1f0ae132c7088a3f475f95603eb
Gabriel <eu@gabrielf.com>
2019-08-09 12:24:03 -0300

    Implementing the ETL process

--- 3-data-lakes-with-spark/L4_Project/etl.py
+++ 3-data-lakes-with-spark/L4_Project/etl.py
@@ -2,12 +2,14 @@ import configparser
 from datetime import datetime
 import os
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql import types as T
 from pyspark.sql.functions import udf, col
 from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
 
 
 config = configparser.ConfigParser()
-config.read('dl.cfg.example')
+config.read('dl.cfg')
 
 os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
 os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
@@ -23,53 +25,138 @@ def create_spark_session():
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
-    song_data = 
+    song_data = input_data + "song_data/"
     
     # read song data file
-    df = 
+    df = (
+        spark.read.schema(T.StructType([
+            T.StructField("num_songs", T.IntegerType(), True),
+            T.StructField("artist_id", T.StringType(), True),
+            T.StructField("artist_latitude", T.DoubleType(), True),
+            T.StructField("artist_longitude", T.DoubleType(), True),
+            T.StructField("artist_location", T.StringType(), True),
+            T.StructField("artist_name", T.StringType(), True),
+            T.StructField("song_id", T.StringType(), True),
+            T.StructField("title", T.StringType(), True),
+            T.StructField("duration", T.DoubleType(), True),
+            T.StructField("year", T.IntegerType(), True),
+        ])).json(song_data)
+    )
 
     # extract columns to create songs table
-    songs_table = 
+    songs_table = (
+        df.select(
+            'song_id', 'title', 'artist_id',
+            'year', 'duration'
+        ).groupBy('song_id')
+    )
     
     # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table.write.parquet(output_data + "songs.parquet", overwrite=True)
 
     # extract columns to create artists table
-    artists_table = 
+    artists_table = (
+        df.select(
+            'artist_id', 'name', 'location',
+            'latitude', 'longitude',
+        ).groupBy('artist_id')
+    )
     
     # write artists table to parquet files
-    artists_table
+    artists_table.write.parquet(output_data + "artists.parquet", overwrite=True)
 
 
 def process_log_data(spark, input_data, output_data):
     # get filepath to log data file
-    log_data =
+    log_data = input_data + "log_data/"
 
     # read log data file
-    df = 
-    
+    df = (
+        spark.read.schema(T.StructType([
+            T.StructField("artist", T.StringType(), True),
+            T.StructField("auth", T.StringType(), True),
+            T.StructField("firstName", T.StringType(), True),
+            T.StructField("gender", T.StringType(), True),
+            T.StructField("itemInSession", T.IntegerType(), True),
+            T.StructField("lastName", T.StringType(), True),
+            T.StructField("length", T.DoubleType(), True),
+            T.StructField("level", T.StringType(), True),
+            T.StructField("location", T.StringType(), True),
+            T.StructField("method", T.StringType(), True),
+            T.StructField("page", T.StringType(), True),
+            T.StructField("registration", T.DoubleType(), True),
+            T.StructField("sessionId", T.IntegerType(), True),
+            T.StructField("song", T.StringType(), True),
+            T.StructField("status", T.IntegerType(), True),
+            T.StructField("ts", T.LongType(), True),
+            T.StructField("userAgent", T.StringType(), True),
+            T.StructField("userId", T.StringType(), True),
+        ])).json(log_data)
+    )
+
     # filter by actions for song plays
-    df = 
+    df = df.where(df.page == 'NextSong')
 
     # extract columns for users table    
-    artists_table = 
+    users_table = (
+        df.select(
+            col('userId').alias('user_id'),
+            col('firstName').alias('first_name'),
+            col('lastName').alias('last_name'),
+            col('gender').alias('gender'),
+            col('level').alias('level')
+        ).groupBy('userId')
+
+    )
     
     # write users table to parquet files
-    artists_table
+    users_table.write.parquet(output_data + "users.parquet", overwrite=True)
 
     # create timestamp column from original timestamp column
-    get_timestamp = udf()
-    df = 
+    # get_timestamp = udf()
+    df = df.withColumn(
+        "ts_timestamp",
+        F.to_timestamp(F.from_unixtime((col("ts") / 1000) , 'yyyy-MM-dd HH:mm:ss.SSS')).cast("Timestamp")
+    )
     
     # create datetime column from original timestamp column
-    get_datetime = udf()
-    df = 
+    # get_datetime = udf()
+    df = df.withColumn(
+        "ts_datetime",
+        F.to_timestamp(F.from_unixtime((col("ts") / 1000), 'yyyy-MM-dd HH:mm:ss.SSS')).cast("DateTime")
+    )
+
+    def get_weekday(date):
+        import datetime
+        import calendar
+        date = date.split(' ')[0]
+        month, day, year = (int(x) for x in date.split('-'))
+        weekday = datetime.date(year, month, day)
+        return calendar.day_name[weekday.weekday()]
+
+    udf_week_day = udf(get_weekday, T.StringType())
     
     # extract columns to create time table
-    time_table = 
+    time_table = (
+        df.withColumn("hour", hour(col("ts_timestamp")))
+          .withColumn("day", day(col("ts_timestamp")))
+          .withColumn("week", weekofyear(col("ts_timestamp")))
+          .withColumn("month", month(col("ts_timestamp")))
+          .withColumn("year", year(col("ts_timestamp")))
+          .withColumn("weekday", udf_week_day(col("ts_timestamp")))
+          .select(
+            col("ts_timestamp").alias("start_time"),
+            col("hour"),
+            col("day"),
+            col("week"),
+            col("month"),
+            col("year"),
+            col("weekday")
+          )
+    )
     
     # write time table to parquet files partitioned by year and month
-    time_table
+    time_table.write.parquet(output_data + "time.parquet", overwrite=True)
 
     # read in song data to use for songplays table
     song_df = 
