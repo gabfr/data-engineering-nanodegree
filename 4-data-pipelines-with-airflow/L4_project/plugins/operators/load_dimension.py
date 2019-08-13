691a370cd1b0069651e51610d82f39d809e509bc
Gabriel <eu@gabrielf.com>
2019-08-13 15:34:19 -0300

    Implementing the dimension operator

--- 4-data-pipelines-with-airflow/L4_project/plugins/operators/load_dimension.py
+++ 4-data-pipelines-with-airflow/L4_project/plugins/operators/load_dimension.py
@@ -5,18 +5,31 @@ from airflow.utils.decorators import apply_defaults
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
+    insert_into_stmt = """
+        INSERT INTO {table} 
+        {select_query}
+    """
 
     @apply_defaults
     def __init__(self,
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
+                 redshift_conn_id,
+                 table,
+                 select_query,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.select_query = select_query
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        redshift.run(LoadDimensionOperator.insert_into_stmt.format(
+            table=self.table,
+            select_query=self.select_query
+        ))
