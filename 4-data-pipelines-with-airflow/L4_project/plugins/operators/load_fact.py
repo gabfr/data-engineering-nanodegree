77a7483bb471f8e8b074a4545e0db1673909cadd
Gabriel <eu@gabrielf.com>
2019-08-13 15:23:29 -0300

    Implementing the LoadFactOperator

--- 4-data-pipelines-with-airflow/L4_project/plugins/operators/load_fact.py
+++ 4-data-pipelines-with-airflow/L4_project/plugins/operators/load_fact.py
@@ -5,18 +5,32 @@ from airflow.utils.decorators import apply_defaults
 class LoadFactOperator(BaseOperator):
 
     ui_color = '#F98866'
+    insert_into_stmt = """
+        INSERT INTO {table} 
+        {select_query}
+    """
 
     @apply_defaults
     def __init__(self,
                  # Define your operators params (with defaults) here
                  # Example:
                  # conn_id = your-connection-name
+                 redshift_conn_id,
+                 table,
+                 select_query,
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.select_query = select_query
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("Clearing data from destination Redshift table")
+        redshift.run(LoadFactOperator.insert_into_stmt.format(
+            table=self.table,
+            select_query=self.select_query
+        ))
