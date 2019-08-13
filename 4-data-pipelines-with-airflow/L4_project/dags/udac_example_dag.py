4085236e249c9e3eb8d6a36172961cc23c03535b
Gabriel <eu@gabrielf.com>
2019-08-13 14:37:32 -0300

    Starting by ordering all the operators execution by the specified project instructions

--- 4-data-pipelines-with-airflow/L4_project/dags/udac_example_dag.py
+++ 4-data-pipelines-with-airflow/L4_project/dags/udac_example_dag.py
@@ -63,3 +63,26 @@ run_quality_checks = DataQualityOperator(
 )
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+# Step 1
+start_operator >> stage_events_to_redshift
+start_operator >> stage_songs_to_redshift
+
+# Step 2
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+
+# Step 3
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+
+# Step 4
+load_song_dimension_table >> run_quality_checks
+load_user_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+
+# Step 5 - end
+run_quality_checks >> end_operator
