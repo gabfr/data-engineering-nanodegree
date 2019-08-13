cdb772628cc592e10c39c7beadd5712c4d083e8c
Gabriel <eu@gabrielf.com>
2019-08-13 11:48:36 -0300

    Removing done TODOs from exercise 4

--- 4-data-pipelines-with-airflow/L3_exercises/exercise4.py
+++ 4-data-pipelines-with-airflow/L3_exercises/exercise4.py
@@ -8,22 +8,9 @@ from airflow.operators import (
     S3ToRedshiftOperator
 )
 
-#
-# TODO: Create a DAG which performs the following functions:
-#
-#       1. Loads Trip data from S3 to RedShift
-#       2. Performs a data quality check on the Trips table in RedShift
-#       3. Uses the FactsCalculatorOperator to create a Facts table in Redshift
-#           a. **NOTE**: to complete this step you must complete the FactsCalcuatorOperator
-#              skeleton defined in plugins/operators/facts_calculator.py
-#
+
 dag = DAG("lesson3.exercise4", start_date=datetime.datetime.utcnow())
 
-#
-# TODO: Load trips data from S3 to RedShift. Use the s3_key
-#       "data-pipelines/divvy/unpartitioned/divvy_trips_2018.csv"
-#       and the s3_bucket "udacity-dend"
-#
 copy_trips_task = S3ToRedshiftOperator(
     task_id="load_trips_from_s3_to_redshift",
     dag=dag,
@@ -34,9 +21,6 @@ copy_trips_task = S3ToRedshiftOperator(
     s3_key="data-pipelines/divvy/unpartitioned/divvy_trips_2018.csv"
 )
 
-#
-# TODO: Perform a data quality check on the Trips table
-#
 check_trips = HasRowsOperator(
     task_id='check_trips_data',
     dag=dag,
@@ -44,11 +28,6 @@ check_trips = HasRowsOperator(
     table="trips"
 )
 
-#
-# TODO: Use the FactsCalculatorOperator to create a Facts table in RedShift. The fact column should
-#       be `tripduration` and the groupby_column should be `bikeid`
-#
-#calculate_facts = FactsCalculatorOperator(...)
 calculate_facts = FactsCalculatorOperator(
     task_id='calculate_facts',
     dag=dag,
@@ -59,8 +38,5 @@ calculate_facts = FactsCalculatorOperator(
     groupby_column="bikeid",
 )
 
-#
-# TODO: Define task ordering for the DAG tasks you defined
-#
 copy_trips_task >> check_trips
 check_trips >> calculate_facts
