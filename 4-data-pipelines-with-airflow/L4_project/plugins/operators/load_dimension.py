fd293ef5feb4d7a59c5e6033e635288cbc45413a
Gabriel <eu@gabrielf.com>
2019-08-14 10:12:13 -0300

    Adding the option to truncate the dimension table before inserting new data

--- 4-data-pipelines-with-airflow/L4_project/plugins/operators/load_dimension.py
+++ 4-data-pipelines-with-airflow/L4_project/plugins/operators/load_dimension.py
@@ -5,6 +5,9 @@ from airflow.utils.decorators import apply_defaults
 class LoadDimensionOperator(BaseOperator):
 
     ui_color = '#80BD9E'
+    truncate_stmt = """
+        TRUNCATE TABLE {table}
+    """
     insert_into_stmt = """
         INSERT INTO {table} 
         {select_query}
@@ -18,6 +21,7 @@ class LoadDimensionOperator(BaseOperator):
                  redshift_conn_id,
                  table,
                  select_query,
+                 truncate_table=False,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
@@ -25,10 +29,18 @@ class LoadDimensionOperator(BaseOperator):
         self.redshift_conn_id = redshift_conn_id
         self.table = table
         self.select_query = select_query
+        self.truncate_table = truncate_table
 
     def execute(self, context):
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
+        if self.truncate_table:
+            self.log.info("Will truncate table before inserting new data...")
+            redshift.run(LoadDimensionOperator.truncate_stmt.format(
+                table=self.table
+            ))
+
+        self.log.info("Inserting dimension table data...")
         redshift.run(LoadDimensionOperator.insert_into_stmt.format(
             table=self.table,
             select_query=self.select_query
